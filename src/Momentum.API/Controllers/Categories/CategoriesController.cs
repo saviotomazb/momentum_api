88b7b0cd0226b3e3c61410ee2f4dee89ab36fc9d
Savio Tomaz <saviotomaz_@hotmail.com>
2026-08-10 06:51:59 -0300

    feat(category): complete category controller

--- src/Momentum.API/Controllers/Categories/CategoriesController.cs
+++ src/Momentum.API/Controllers/Categories/CategoriesController.cs
@@ -29,7 +29,7 @@ public class CategoriesController : ControllerBase
             return Unauthorized();
         }
 
-        if(!Guid.TryParse(userIdClaim.Value, out var userId))
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
         {
             return Unauthorized();
         }
@@ -37,5 +37,90 @@ public class CategoriesController : ControllerBase
         var category = await _categoryService.CreateAsync(userId, request);
 
         return Ok(category);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryResponse>>> GetAll()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim is null)
+        {
+            return Unauthorized();
+        }
+
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        var categories = await _categoryService.GetAllAsync(userId);
+
+        return Ok(categories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CategoryResponse>> GetById(Guid id)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim is null)
+        {
+            return Unauthorized();
+        }
+
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        var category = await _categoryService.GetByIdAsync(userId, id);
+
+        return Ok(category);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<CategoryResponse>> Update(
+        Guid id,
+        [FromBody] UpdateCategoryRequest request)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim is null)
+        {
+            return Unauthorized();
+        }
+
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        var category = await _categoryService.UpdateAsync(
+            userId,
+            id,
+            request);
+
+        return Ok(category);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim is null)
+        {
+            return Unauthorized();
+        }
+
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        await _categoryService.DeleteAsync(userId, id);
+
+        return NoContent();
     }    
 }
